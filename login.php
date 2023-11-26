685a2ba0acbc31d7f60d69d39024868196e140bf
Danh Pham <danhpham612@gmail.com>
2023-11-26 13:40:05 -0800

    add salt authentication and sql placeholder

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
   session_start();
   require_once 'dbconnect.php';
+
   if( isset($_POST['signup']) && $_POST["username"] != '' && $_POST["password"] != ''){ // sign up section
 
     $username = $_POST['username'];
@@ -13,15 +14,20 @@
 
     } else {
 
+      function generateRandomSalt($length = 16) {
+        return bin2hex(random_bytes($length));
+      }
+
       $username = $_POST['username'];
       $password = $_POST['password'];
-      $salt1 = "e#fna$%#2";
-      $salt2 = "243rwe890";
 
-      $newPassword = md5($salt1 . $password . $salt2); // encrypted password
+      $salt = generateRandomSalt();
+      $newPassword = md5($salt . $password); // encrypted password
 
-      $sql = "INSERT INTO user_info (user_name, hashed_password) VALUES ('$username', '$newPassword')";
-      $query = mysqli_query($conn,$sql);
+      $sql = "INSERT INTO user_info (user_name, hashed_password, salt) VALUES (?, ?, ?)";
+      $stmt = mysqli_prepare($conn, $sql);
+      mysqli_stmt_bind_param($stmt, 'sss', $username, $newPassword, $salt);
+      $query = mysqli_stmt_execute($stmt);
       
       if($query){ 
         echo"<script>alert('Account Registered Successfully')</script>";
@@ -33,18 +39,27 @@
   if( isset($_POST['login']) && $_POST["username"] != '' && $_POST["password"] != ''){ // login section
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $salt1 = "e#fna$%#2";
-    $salt2 = "243rwe890";
-    $password = md5($salt1 . $password . $salt2); // encrypted password
 
-    $sql = "SELECT * FROM user_info WHERE user_name='$username' AND hashed_password='$password'";
-    $Credentials = mysqli_query($conn,$sql);
+    $sql = "SELECT * FROM user_info WHERE user_name=?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 's', $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
 
-    if (mysqli_num_rows($Credentials) > 0){ // check if query return 1 row mean username and pw are correct
-      $_SESSION["user"] = $username;
-      header("location:home.php");
+    if ($row = mysqli_fetch_assoc($result)) {
+      // Combine the retrieved salt with the entered password and hash
+      $saltedPassword = md5($row['salt'] . $password);
+
+      // Check if the calculated hash matches the stored hashed password
+      if ($saltedPassword === $row['hashed_password']) {
+        $_SESSION["user"] = $username;
+        header("location:home.php");
+      } else {
+        echo "Username or password was wrong";
+      }
+
     } else {
-      echo"username or password was wrong";
+      echo "Username or password was wrong";
     }
   }
 ?>
